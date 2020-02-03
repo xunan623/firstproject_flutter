83131a747f827248f726db6aa8f94f3374c7f44a
xunan <xunan623@163.com>
2020-02-03 09:07:22 +0800

    抽屉页面

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,94 @@
 import 'package:flutter/material.dart';
-import 'model/post.dart';
+import './demo/listview_demo.dart';
+import './demo/hello_demo.dart';
 
 void main() => runApp(App());
 class App extends StatelessWidget {
   @override 
    Widget build(BuildContext context) {
      return MaterialApp(
+       debugShowCheckedModeBanner: false, // 是否显示调试flutter右上角的默认图
        home: Home(),
        theme: ThemeData(
-         primarySwatch: Colors.yellow
+         primarySwatch: Colors.yellow, // 导航条背景色
+         highlightColor: Color.fromRGBO(255, 255, 255, 0.5), // 主题高亮颜色
+         splashColor: Colors.white70, // 点击按钮水波纹颜色
        )
      );
    }
 }
 
 class Home extends StatelessWidget {
-  Widget _listItemBuilder(BuildContext context, int index) {
-    return Container(
-      color: Colors.white,
-      margin: EdgeInsets.all(8.0),
-      child: Column(
-        children: <Widget>[
-          Image.network(posts[index].imageUrl),
-          SizedBox(height: 16.0),
-          Text(
-            posts[index].title,
-            style: Theme.of(context).textTheme.title,
-          ),
-          Text(
-            posts[index].author,
-            style: Theme.of(context).textTheme.subhead,
-          ),
-          SizedBox(height: 16.0),
-        ],
-      ),
-    );
-  }
-
   @override
     Widget build(BuildContext context) {
-      return Scaffold(
+      return DefaultTabController(
+        length: 3,
+        child: Scaffold(
         backgroundColor: Colors.grey[100],
-         appBar: AppBar(
-           title: Text('您好'),
+        appBar: AppBar(
+          //  leading: IconButton( // 自定义左侧按钮
+          //    icon: Icon(Icons.menu),
+          //    tooltip: 'Navigation',
+          //    onPressed: () => debugPrint('Navigation button is pressed'),
+          //  ),
+           title: Text('flutter...'),
+           actions: <Widget>[ 
+              IconButton(
+                icon: Icon(Icons.search),
+                tooltip: 'Navigation',
+                onPressed: () => debugPrint('Navigation search is pressed'),
+              ),
+           ],
            elevation: 1.0,
+           bottom: TabBar(
+             unselectedLabelColor: Colors.black38, // 未选中的图标颜色
+             indicatorColor: Colors.black54, // 下划线颜色
+             indicatorSize: TabBarIndicatorSize.label, // 下划线长度
+             indicatorWeight: 1.0, // 下划线粗细
+             tabs: <Widget>[
+               Tab(icon: Icon(Icons.local_florist)),
+               Tab(icon: Icon(Icons.change_history)),
+               Tab(icon: Icon(Icons.directions_bike)),
+             ],
+           ),
          ),
-         body: ListView.builder(
-           itemCount: posts.length,
-           itemBuilder: _listItemBuilder,
+        body: TabBarView(
+           children: <Widget>[
+             Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
+             Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+             Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
+           ],
          ),
-       );
-    }
-}
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('header'.toUpperCase()),
+                decoration: BoxDecoration(
+                  color: Colors.grey[100],
+                ),
+              ),
+              ListTile(
+                title: Text('Messages', textAlign: TextAlign.right),
+                trailing: Icon(Icons.message, color: Colors.black12, size: 22.0,),
+                onTap: () => Navigator.pop(context),
+              ),
+              ListTile(
+                title: Text('Favorite', textAlign: TextAlign.right),
+                trailing: Icon(Icons.favorite, color: Colors.black12, size: 22.0,),
+                onTap: () => Navigator.pop(context),
+              ),
+              ListTile(
+                title: Text('Setting', textAlign: TextAlign.right),
+                trailing: Icon(Icons.settings, color: Colors.black12, size: 22.0,), 
+                onTap: () => Navigator.pop(context),
+              ),
 
-class Hello extends StatelessWidget {
-    @override 
-   Widget build(BuildContext context) {
-     return Center(
-      child: Text(
-        '狗哥',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-          fontSize: 40.0,
-          fontWeight: FontWeight.bold,
-          color: Colors.black87,
-        ),
-      ),
-    );
-   }
+            ],
+          ),
+        ) // 左边抽屉drawer  右边抽屉endDrawer
+       )
+      );
+    }
 }
