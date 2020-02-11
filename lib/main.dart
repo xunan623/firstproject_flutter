35d0b56ea4455cf400f0abeac9f4a142d1aad233
xunan <xunan623@163.com>
2020-02-11 16:51:23 +0800

    RxDart

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import './demo/form_demo.dart';
 import './demo/material_components.dart';
 import './demo/state/state_management_demo.dart';
 import './demo/stream/stream_demo.dart';
+import './demo/rxdart/rxdart_demo.dart';
 
 void main() => runApp(App());
 class App extends StatelessWidget {
@@ -21,7 +22,7 @@ class App extends StatelessWidget {
      return MaterialApp(
        debugShowCheckedModeBanner: false, // 是否显示调试flutter右上角的默认图
       //  home: Home(),
-       initialRoute: '/stream', 
+       initialRoute: '/rxdart', 
        routes: {
         //  '/': (context) => NavigatorDemo(), // 报错
         '/about': (context) => Page(title: 'About'),
@@ -29,6 +30,7 @@ class App extends StatelessWidget {
         '/mdc': (context) => MaterialComponments(),
         '/state-management': (context) => StateManagementDemo(),
         '/stream' : (context) => StreamDemo(),
+        '/rxdart' : (context) => RxDartDemo(),
        },
        theme: ThemeData(
          primarySwatch: Colors.yellow, // 导航条背景色
