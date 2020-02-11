643bdcd77afcef5bae356279e7947cc8bff1b58d
xunan <xunan623@163.com>
2020-02-12 00:41:06 +0800

    网络请求

--- lib/main.dart
+++ lib/main.dart
@@ -15,85 +15,85 @@ import './demo/state/state_management_demo.dart';
 import './demo/stream/stream_demo.dart';
 import './demo/rxdart/rxdart_demo.dart';
 import './demo/bloc/bloc_demo.dart';
+import './demo/http/http_demo.dart';
 
 void main() => runApp(App());
+
 class App extends StatelessWidget {
-  @override 
-   Widget build(BuildContext context) {
-     return MaterialApp(
-       debugShowCheckedModeBanner: false, // 是否显示调试flutter右上角的默认图
-      //  home: Home(),
-       initialRoute: '/bloc', 
-       routes: {
-        //  '/': (context) => NavigatorDemo(), // 报错
-        '/about': (context) => Page(title: 'About'),
-        '/form': (context) => FormDemo(),
-        '/mdc': (context) => MaterialComponments(),
-        '/state-management': (context) => StateManagementDemo(),
-        '/stream' : (context) => StreamDemo(),
-        '/rxdart' : (context) => RxDartDemo(),
-        '/bloc' : (context) => BlocDemo(),
-       },
-       theme: ThemeData(
-         primarySwatch: Colors.yellow, // 导航条背景色
-         highlightColor: Color.fromRGBO(255, 255, 255, 0.5), // 主题高亮颜色
-         splashColor: Colors.white70, // 点击按钮水波纹颜色
-         accentColor:  Color.fromRGBO(3, 54, 255, 1.0),
-       )
-     );
-   }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false, // 是否显示调试flutter右上角的默认图
+        //  home: Home(),
+        initialRoute: '/http',
+        routes: {
+          //  '/': (context) => NavigatorDemo(), // 报错
+          '/about': (context) => Page(title: 'About'),
+          '/form': (context) => FormDemo(),
+          '/mdc': (context) => MaterialComponments(),
+          '/state-management': (context) => StateManagementDemo(),
+          '/stream': (context) => StreamDemo(),
+          '/rxdart': (context) => RxDartDemo(),
+          '/bloc': (context) => BlocDemo(),
+          '/http': (context) => HttpDemo(),
+        },
+        theme: ThemeData(
+          primarySwatch: Colors.yellow, // 导航条背景色
+          highlightColor: Color.fromRGBO(255, 255, 255, 0.5), // 主题高亮颜色
+          splashColor: Colors.white70, // 点击按钮水波纹颜色
+          accentColor: Color.fromRGBO(3, 54, 255, 1.0),
+        ));
+  }
 }
 
 class Home extends StatelessWidget {
-
   @override
-    Widget build(BuildContext context) {
-      return DefaultTabController(
+  Widget build(BuildContext context) {
+    return DefaultTabController(
         length: 5,
         child: Scaffold(
-        backgroundColor: Colors.grey[100],
-        appBar: AppBar(
-          //  leading: IconButton( // 自定义左侧按钮
-          //    icon: Icon(Icons.menu),
-          //    tooltip: 'Navigation',
-          //    onPressed: () => debugPrint('Navigation button is pressed'),
-          //  ),
-           title: Text('flutter...'),
-           actions: <Widget>[ 
+          backgroundColor: Colors.grey[100],
+          appBar: AppBar(
+            //  leading: IconButton( // 自定义左侧按钮
+            //    icon: Icon(Icons.menu),
+            //    tooltip: 'Navigation',
+            //    onPressed: () => debugPrint('Navigation button is pressed'),
+            //  ),
+            title: Text('flutter...'),
+            actions: <Widget>[
               IconButton(
                 icon: Icon(Icons.search),
                 tooltip: 'Navigation',
                 onPressed: () => debugPrint('Navigation search is pressed'),
               ),
-           ],
-           elevation: 1.0,
-           bottom: TabBar(
-             unselectedLabelColor: Colors.black38, // 未选中的图标颜色
-             indicatorColor: Colors.black54, // 下划线颜色
-             indicatorSize: TabBarIndicatorSize.label, // 下划线长度
-             indicatorWeight: 1.0, // 下划线粗细
-             tabs: <Widget>[
-               Tab(icon: Icon(Icons.local_florist)),
-               Tab(icon: Icon(Icons.change_history)),
-               Tab(icon: Icon(Icons.directions_bike)),
-               Tab(icon: Icon(Icons.view_quilt)),
-               Tab(icon: Icon(Icons.shuffle)),
-             ],
-           ),
-         ),
-        body: TabBarView(
-           children: <Widget>[
-            ListViewDemo(),
-            BasicDemo(),
-            LayoutDemo(),
-            ViewDemo(),
-            SliverDemo(),
-            //  Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
-           ],
-         ),
-        drawer: DrawerDemo(),
-        bottomNavigationBar: BottomNavigationBarDemo(),
-       )
-      );
-    }
+            ],
+            elevation: 1.0,
+            bottom: TabBar(
+              unselectedLabelColor: Colors.black38, // 未选中的图标颜色
+              indicatorColor: Colors.black54, // 下划线颜色
+              indicatorSize: TabBarIndicatorSize.label, // 下划线长度
+              indicatorWeight: 1.0, // 下划线粗细
+              tabs: <Widget>[
+                Tab(icon: Icon(Icons.local_florist)),
+                Tab(icon: Icon(Icons.change_history)),
+                Tab(icon: Icon(Icons.directions_bike)),
+                Tab(icon: Icon(Icons.view_quilt)),
+                Tab(icon: Icon(Icons.shuffle)),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              ListViewDemo(),
+              BasicDemo(),
+              LayoutDemo(),
+              ViewDemo(),
+              SliverDemo(),
+              //  Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
+            ],
+          ),
+          drawer: DrawerDemo(),
+          bottomNavigationBar: BottomNavigationBarDemo(),
+        ));
+  }
 }
